125142a87d2553832f856277d461f7604598a0d9
= <=>
2018-09-14 17:05:18 -0500

    cargar imágenes

--- app/src/main/java/com/maycc/examplefoursquare/FrqResponse.kt
+++ app/src/main/java/com/maycc/examplefoursquare/FrqResponse.kt
@@ -4,4 +4,8 @@ class FrqResponse(var response: Response)
 
 class Response(var venues: ArrayList<Venue>)
 
-class Venue(var id: String, var name: String)
+class Venue(var id: String, var name: String, var categories: ArrayList<Category>)
+
+class Category(var id: String, var icon: Icon)
+
+class Icon(var prefix: String, var suffix: String)
--- app/src/main/java/com/maycc/examplefoursquare/VenueAdapter.kt
+++ app/src/main/java/com/maycc/examplefoursquare/VenueAdapter.kt
@@ -4,7 +4,9 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_layout_venue.view.*
 
 class VenueAdapter(var venues: ArrayList<Venue>) : RecyclerView.Adapter<VenueAdapter.ViewHolder>(){
@@ -21,11 +23,23 @@ class VenueAdapter(var venues: ArrayList<Venue>) : RecyclerView.Adapter<VenueAda
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val venue = venues[position]
 
+        val icon = venue.categories[0].icon
+        val iconUrl = getIconUrl(icon)
+
         holder.tvNameVenue.text = venue.name
+        Picasso.get().load(iconUrl).into(holder.ivVenue)
+    }
+
+    private fun getIconUrl(icon: Icon): String {
+        val prefix = icon.prefix
+        val suffix = icon.suffix
+
+        return prefix + "bg_64" + suffix
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val tvNameVenue:TextView = itemView.tv_name_venue
+        val ivVenue: ImageView   = itemView.iv_venue
     }
 
 }
